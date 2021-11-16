e7deb677c961b54f52c6c0f0e6ac8e91dcc91f71
Tebogo <elveranosa@gmail.com>
2021-11-16 20:28:06 +0200

    tests

--- test/fruit.test.js
+++ test/fruit.test.js
@@ -1,12 +1,10 @@
-'use strict'
-
 const assert = require('assert');
 const factoryFun = require('../fruitFactory')
 
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://tebogo:12345@localhost:5432/my_database';;
+const connectionString = process.env.DATABASE_URL || 'postgres://tebza:12345@localhost:5432/mybasket';
 
 const pool = new Pool({
     connectionString,
@@ -19,16 +17,24 @@ const factoryBasket = factoryFun(pool);
 
 describe('Fruit Basket', function(){
     beforeEach(async function(){
-        await pool.query('delete from fruit_basket;');
+        await pool.query('DELETE FROM fruit_basket;');
     });
 
 it('it should insert/create new basket', async function(){
     await factoryBasket.CreateBasket('Pear', 20, 5)
+    await factoryBasket.CreateBasket('Banana', 10, 25)
+
     const from = await (await pool.query ('SELECT * FROM fruit_basket')).rows[0];
 
     assert.equal(from.fruit, 'Pear');
     assert.equal(from.quantity, 20);
     assert.equal(from.unit_price, 5);
+
+    const from2 = await (await pool.query ('SELECT * FROM fruit_basket')).rows[1];
+
+    assert.equal(from2.fruit, 'Banana');
+    assert.equal(from2.quantity, 10);
+    assert.equal(from2.unit_price, 25);
 })
 
 });
